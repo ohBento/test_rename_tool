dce2efc56263217a8d870ce9e527d384bca61097
ohBento <w4angm4ndu@gmail.com>
2023-08-29 13:35:20 +0200

    init commit
    init commit

---
+++ test_rename_tool/test_rename_tool/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace test_rename_tool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
